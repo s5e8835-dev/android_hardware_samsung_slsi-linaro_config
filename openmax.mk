529a18967f53d3631a010cd09cc4c4f3e3869f97
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    config: Update some soong config variables to bool type
    Change-Id: Idb2688ba9f616b03e60f5786ed82f7102112e233
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- openmax.mk
+++ openmax.mk
@@ -21,18 +21,18 @@ EXYNOS_OMX_SUPPORT_EGL_IMAGE := false
 
 # only 3.4 kernel
 ifeq ($(filter-out 3.4, $(TARGET_LINUX_KERNEL_VERSION)),)
-$(call soong_config_set,openmax,USE_EXYNOS_MEDIA_EXT,true)
+$(call soong_config_set_bool,openmax,USE_EXYNOS_MEDIA_EXT,true)
 endif
 
 # since 3.10 kernel
 ifneq ($(filter-out 3.4, $(TARGET_LINUX_KERNEL_VERSION)),)
-$(call soong_config_set,openmax,USE_DEFINE_H264_SEI_TYPE,true)
+$(call soong_config_set_bool,openmax,USE_DEFINE_H264_SEI_TYPE,true)
 endif
 
 # since 3.18 kernel
 ifneq ($(filter-out 3.4 3.10, $(TARGET_LINUX_KERNEL_VERSION)),)
-$(call soong_config_set,openmax,USE_ORIGINAL_HEADER,true)
-$(call soong_config_set,openmax,USE_MFC_HEADER,true)
+$(call soong_config_set_bool,openmax,USE_ORIGINAL_HEADER,true)
+$(call soong_config_set_bool,openmax,USE_MFC_HEADER,true)
 endif
 
 # since 4.19 kernel
@@ -40,25 +40,25 @@ ifdef TARGET_LINUX_KERNEL_VERSION
 KERNEL_VERSION_MAJOR := $(word 1,$(subst ., ,$(TARGET_LINUX_KERNEL_VERSION)))
 KERNEL_VERSION_MINOR := $(word 2,$(subst ., ,$(TARGET_LINUX_KERNEL_VERSION)))
 ifeq ($(shell expr $(KERNEL_VERSION_MAJOR) \> 4), 1)
-$(call soong_config_set,openmax,MAINLINE_FEATURE_IN_SINCE_4_19,true)
+$(call soong_config_set_bool,openmax,MAINLINE_FEATURE_IN_SINCE_4_19,true)
 else ifeq ($(shell expr $(KERNEL_VERSION_MAJOR) = 4), 1)
 ifeq ($(shell expr $(KERNEL_VERSION_MINOR) \>= 19), 1)
-$(call soong_config_set,openmax,MAINLINE_FEATURE_IN_SINCE_4_19,true)
+$(call soong_config_set_bool,openmax,MAINLINE_FEATURE_IN_SINCE_4_19,true)
 endif
 endif
 endif
 
 
 ifeq ($(BOARD_USE_HEVC_HWIP), true)
-$(call soong_config_set,openmax,USE_HEVC_HWIP,true)
+$(call soong_config_set_bool,openmax,USE_HEVC_HWIP,true)
 endif
 
 ifeq ($(BOARD_USE_DEINTERLACING_SUPPORT), true)
-$(call soong_config_set,openmax,USE_DEINTERLACING_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_DEINTERLACING_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_USE_SINGLE_PLANE_IN_DRM), true)
-$(call soong_config_set,openmax,USE_SINGLE_PLANE_IN_DRM,true)
+$(call soong_config_set_bool,openmax,USE_SINGLE_PLANE_IN_DRM,true)
 endif
 
 ifneq ($(BOARD_USE_FRAMERATE_THRESH_HOLD),)
@@ -70,15 +70,15 @@ $(call soong_config_set,openmax,S10B_FORMAT_8B_ALIGNMENT,$(BOARD_EXYNOS_S10B_FOR
 endif
 
 ifeq ($(BOARD_USE_DMA_BUF), true)
-$(call soong_config_set,openmax,USE_DMA_BUF,true)
+$(call soong_config_set_bool,openmax,USE_DMA_BUF,true)
 endif
 
 ifeq ($(BOARD_USE_CSC_HW), true)
-$(call soong_config_set,openmax,USE_CSC_HW,true)
+$(call soong_config_set_bool,openmax,USE_CSC_HW,true)
 endif
 
 ifeq ($(BOARD_USE_NON_CACHED_GRAPHICBUFFER), true)
-$(call soong_config_set,openmax,USE_NON_CACHED_GRAPHICBUFFER,true)
+$(call soong_config_set_bool,openmax,USE_NON_CACHED_GRAPHICBUFFER,true)
 endif
 
 ifdef BOARD_MFC_CHROMA_VALIGN
@@ -88,39 +88,39 @@ $(call soong_config_set,openmax,CHROMA_VALIGN,1)
 endif
 
 ifeq ($(BOARD_USE_WA_ION_BUF_REF), true)
-$(call soong_config_set,openmax,USE_WA_ION_BUF_REF,true)
+$(call soong_config_set_bool,openmax,USE_WA_ION_BUF_REF,true)
 endif
 
 ifeq ($(BOARD_USES_EXYNOS_GRALLOC_VERSION), 3)
-$(call soong_config_set,openmax,USE_WA_ION_BUF_REF,true)
+$(call soong_config_set_bool,openmax,USE_WA_ION_BUF_REF,true)
 endif
 
 ifeq ($(BOARD_OMX_USES_EPIC), true)
-$(call soong_config_set,openmax,OMX_USES_EPIC,true)
+$(call soong_config_set_bool,openmax,OMX_USES_EPIC,true)
 endif
 
 ifeq ($(BOARD_USES_EXYNOS_DATASPACE_FEATURE), true)
-$(call soong_config_set,openmax,USE_BT709_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_BT709_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_USES_EXYNOS_GRALLOC_VERSION), 0)
-$(call soong_config_set,openmax,GRALLOC_VERSION0,true)
-$(call soong_config_set,openmax,USE_PRIV_FORMAT,true)
+$(call soong_config_set_bool,openmax,GRALLOC_VERSION0,true)
+$(call soong_config_set_bool,openmax,USE_PRIV_FORMAT,true)
 else
-$(call soong_config_set,openmax,USE_PRIV_USAGE,true)
+$(call soong_config_set_bool,openmax,USE_PRIV_USAGE,true)
 endif
 
 ifeq ($(BOARD_USE_SKYPE_HD), true)
-$(call soong_config_set,openmax,USE_SKYPE_HD,true)
+$(call soong_config_set_bool,openmax,USE_SKYPE_HD,true)
 endif
 
 ifeq ($(BOARD_USE_FULL_ST2094_40), true)
-$(call soong_config_set,openmax,USE_FULL_ST2094_40,true)
-$(call soong_config_set,exynos_headers_c2,full_st2094_40,true)
+$(call soong_config_set_bool,openmax,USE_FULL_ST2094_40,true)
+$(call soong_config_set_bool,exynos_headers_c2,full_st2094_40,true)
 endif
 
 ifeq ($(BOARD_USE_HDR10PLUS_STAT_ENC), true)
-$(call soong_config_set,openmax,USE_HDR10PLUS_STAT_ENC,true)
+$(call soong_config_set_bool,openmax,USE_HDR10PLUS_STAT_ENC,true)
 endif
 
 ifeq ($(BOARD_HAS_SCALER_ALIGN_RESTRICTION), true)
@@ -130,19 +130,19 @@ $(call soong_config_set,openmax,MSCL_EXT_SIZE,0)
 endif
 
 ifeq ($(EXYNOS_OMX_SUPPORT_EGL_IMAGE), true)
-$(call soong_config_set,openmax,EGL_IMAGE_SUPPORT,true)
+$(call soong_config_set_bool,openmax,EGL_IMAGE_SUPPORT,true)
 endif
 
 ifeq ($(EXYNOS_OMX_SUPPORT_TUNNELING), true)
-$(call soong_config_set,openmax,TUNNELING_SUPPORT,true)
+$(call soong_config_set_bool,openmax,TUNNELING_SUPPORT,true)
 endif
 
 ifneq ($(BOARD_NOT_SUPPORT_DCPP), true)
-$(call soong_config_set,openmax,USE_SECURE_WITH_NONSECURE,true)
+$(call soong_config_set_bool,openmax,USE_SECURE_WITH_NONSECURE,true)
 endif
 
 ifeq ($(BOARD_USE_SINGLE_DRM), true)
-$(call soong_config_set,openmax,USE_SINGLE_DRM,true)
+$(call soong_config_set_bool,openmax,USE_SINGLE_DRM,true)
 endif
 
 ifdef BOARD_USE_MAX_COMPONENT_NUM
@@ -150,72 +150,72 @@ $(call soong_config_set,openmax,MAX_COMPONENT_NUM,$(BOARD_USE_MAX_COMPONENT_NUM)
 endif
 
 ifeq ($(BOARD_USE_SMALL_SECURE_MEMORY), true)
-$(call soong_config_set,openmax,USE_SMALL_SECURE_MEMORY,true)
+$(call soong_config_set_bool,openmax,USE_SMALL_SECURE_MEMORY,true)
 endif
 
 ifeq ($(BOARD_USE_COMPRESSED_COLOR), true)
-$(call soong_config_set,openmax,USE_COMPRESSED_COLOR,true)
+$(call soong_config_set_bool,openmax,USE_COMPRESSED_COLOR,true)
 endif
 
 ifeq ($(BOARD_USE_S3D_SUPPORT), true)
-$(call soong_config_set,openmax,USE_S3D_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_S3D_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_USE_CUSTOM_COMPONENT_SUPPORT), true)
-$(call soong_config_set,openmax,USE_CUSTOM_COMPONENT_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_CUSTOM_COMPONENT_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_USE_VP9DEC_SUPPORT), true)
-$(call soong_config_set,openmax,USE_VP9DEC_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_VP9DEC_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_USE_GSC_RGB_ENCODER), true)
-$(call soong_config_set,openmax,USE_HW_CSC_GRALLOC_SOURCE,true)
+$(call soong_config_set_bool,openmax,USE_HW_CSC_GRALLOC_SOURCE,true)
 endif
 
 ifeq ($(BOARD_USE_FIMC_RGB_ENCODER), true)
-$(call soong_config_set,openmax,USE_FIMC_RGB_ENCODER,true)
+$(call soong_config_set_bool,openmax,USE_FIMC_RGB_ENCODER,true)
 endif
 
 
 ifeq ($(BOARD_USE_VIDEO_EXT_FOR_WFD_HDCP), true)
-$(call soong_config_set,openmax,USE_VIDEO_EXT_FOR_WFD_HDCP,true)
+$(call soong_config_set_bool,openmax,USE_VIDEO_EXT_FOR_WFD_HDCP,true)
 endif
 
 ifeq ($(BOARD_USE_WFDENC_SUPPORT), true)
-$(call soong_config_set,openmax,USE_WFDENC_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_WFDENC_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_USE_HEVCENC_SUPPORT), true)
-$(call soong_config_set,openmax,USE_HEVCENC_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_HEVCENC_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_USE_VP9ENC_SUPPORT), true)
-$(call soong_config_set,openmax,USE_VP9ENC_SUPPORT,true)
+$(call soong_config_set_bool,openmax,USE_VP9ENC_SUPPORT,true)
 endif
 
 ifeq ($(BOARD_DISABLE_RAPID_COMPONENT_LOAD), true)
-$(call soong_config_set,openmax,USE_DISABLE_RAPID_COMPONENT_LOAD,true)
+$(call soong_config_set_bool,openmax,USE_DISABLE_RAPID_COMPONENT_LOAD,true)
 endif
 
 ifeq ($(BOARD_USE_NON_DRM_CODEC_ONLY), true)
-$(call soong_config_set,openmax,USE_NON_DRM_CODEC_ONLY,true)
+$(call soong_config_set_bool,openmax,USE_NON_DRM_CODEC_ONLY,true)
 endif
 
 ifeq ($(BOARD_USE_CSC_FILTER), true)
-$(call soong_config_set,openmax,USE_CSC_FILTER,true)
+$(call soong_config_set_bool,openmax,USE_CSC_FILTER,true)
 endif
 
 ifeq ($(BOARD_SUPPORT_MFC_ENC_RGB), true)
-$(call soong_config_set,openmax,SUPPORT_MFC_ENC_RGB,true)
+$(call soong_config_set_bool,openmax,SUPPORT_MFC_ENC_RGB,true)
 endif
 
 ifeq ($(BOARD_USE_GDC), true)
-$(call soong_config_set,openmax,USE_GDC,true)
+$(call soong_config_set_bool,openmax,USE_GDC,true)
 endif
 
 ifeq ($(BOARD_SUPPORT_MFC_ENC_BT2020), true)
-$(call soong_config_set,openmax,SUPPORT_MFC_ENC_BT2020,true)
+$(call soong_config_set_bool,openmax,SUPPORT_MFC_ENC_BT2020,true)
 endif
 
 ifdef BOARD_GPU_TYPE
@@ -223,15 +223,15 @@ $(call soong_config_set,openmax,GPU_TYPE,$(BOARD_GPU_TYPE))
 endif
 
 ifeq ($(BOARD_USE_DEC_SW_CSC), true)
-$(call soong_config_set,openmax,USE_DEC_SW_CSC,true)
+$(call soong_config_set_bool,openmax,USE_DEC_SW_CSC,true)
 endif
 
 ifeq ($(BOARD_USE_BLOB_ALLOCATOR), true)
-$(call soong_config_set,openmax,USE_BLOB_ALLOCATOR,true)
+$(call soong_config_set_bool,openmax,USE_BLOB_ALLOCATOR,true)
 endif
 
 ifeq ($(BOARD_USE_QUERY_HDR2SDR), true)
-$(call soong_config_set,openmax,USE_QUERY_HDR2SDR,true)
+$(call soong_config_set_bool,openmax,USE_QUERY_HDR2SDR,true)
 endif
 
 ifdef BOARD_HDR_DYNAMIC_META_LIB
@@ -239,13 +239,13 @@ $(call soong_config_set,openmax,HDR_DYNAMIC_META_LIB,$(BOARD_HDR_DYNAMIC_META_LI
 endif
 
 ifeq ($(BOARD_HW_SUPPORT_FILMGRAIN), true)
-$(call soong_config_set,openmax,USE_FILMGRAIN_FILTER,true)
+$(call soong_config_set_bool,openmax,USE_FILMGRAIN_FILTER,true)
 endif
 
 ifeq ($(BOARD_USE_SUPPORT_GPU_SBWC), true)
-$(call soong_config_set,openmax,USE_SUPPORT_GPU_SBWC,true)
+$(call soong_config_set_bool,openmax,USE_SUPPORT_GPU_SBWC,true)
 endif
 
 ifeq ($(BOARD_USE_FLEXIBLE_P010), true)
-$(call soong_config_set,openmax,USE_FLEXIBLE_P010,true)
+$(call soong_config_set_bool,openmax,USE_FLEXIBLE_P010,true)
 endif
